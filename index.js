8da4b9b16e17a2671d867daa8e2d73fef90ef8ad
Nargis21 <nargisakther1933@gmail.com>
2022-05-06 01:00:40 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express())
+
+app.get('/', (req, res) => {
+    res.end('Laptop mart server is running')
+})
+
+app.listen(port, () => {
+    console.log('Listening to port:', port)
+})
+
