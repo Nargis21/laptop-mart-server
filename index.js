cf7c87f9225258f37b20009471331db2ffe8c1d6
Nargis21 <nargisakther1933@gmail.com>
2022-05-06 12:27:49 +0600

    deploy with heroku

--- index.js
+++ index.js
@@ -5,11 +5,36 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
+// middlewares
 app.use(cors())
 app.use(express())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dswlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const productCollection = client.db('laptopMart').collection('product')
+        //   product api
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
-    res.end('Laptop mart server is running')
+    res.send('Laptop mart server is running')
 })
 
 app.listen(port, () => {
