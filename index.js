bf9b1f4bf707b716e5ee45a0380ad5a453b223c1
Nargis21 <nargisakther1933@gmail.com>
2022-05-07 22:41:49 +0600

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express())
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dswlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
 async function run() {
     try {
         await client.connect()
