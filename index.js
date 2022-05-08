83dc264cb1a8e7db59789bfa4659367838df0f71
Nargis21 <nargisakther1933@gmail.com>
2022-05-08 22:01:51 +0600

    added token api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const app = express()
@@ -15,6 +16,21 @@ app.options('*', cors(corsConfig))
 // app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded
+        next()
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dswlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -24,6 +40,14 @@ async function run() {
     try {
         await client.connect()
         const productCollection = client.db('laptopMart').collection('product')
+
+        // Auth api
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send(accessToken)
+        })
+
         // load  product api
         app.get('/product', async (req, res) => {
             const query = {}
@@ -46,22 +70,7 @@ async function run() {
             res.send(product)
         })
 
-        // Update quantity
-        // app.put('/product/:id', async (req, res) => {
-        //     const id = req.params.id
-        //     const updatedProduct = req.body
-        //     console.log(updatedQuantity)
-        //     const filter = { _id: ObjectId(id) }
-        //     const options = { upsert: true }
-        //     const updateDoc = {
-        //         $set: {
-        //             quantity: updatedProduct.updatedQuantity
-        //         }
-        //     }
-        //     const result = await productCollection.updateOne(filter, updateDoc, options)
-        //     res.send(result)
-        // })
-
+        // Update quantity api
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id
             const updatedProduct = req.body
@@ -89,6 +98,20 @@ async function run() {
             const newProduct = req.body
             const result = await productCollection.insertOne(newProduct)
             res.send(result)
+        })
+
+        app.get('/items', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email
+            if (decodedEmail === email) {
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
     }
