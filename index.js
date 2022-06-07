eff768fc35edb19b9e6aff9078a1b4da965e9517
Nargis21 <nargisakther1933@gmail.com>
2022-06-07 15:03:55 +0600

    clean code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const app = express()
@@ -16,22 +15,6 @@ app.options('*', cors(corsConfig))
 // app.use(cors())
 app.use(express.json())
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'unauthorized access' })
-//     }
-//     const token = authHeader.split(' ')[1]
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' })
-//         }
-//         console.log('decoded', decoded)
-//         req.decoded = decoded
-//         next()
-//     })
-// }
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dswlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -39,13 +22,6 @@ async function run() {
     try {
         await client.connect()
         const productCollection = client.db('laptopMart').collection('product')
-
-        // // Auth api
-        // app.post('/login', async (req, res) => {
-        //     const user = req.body
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' })
-        //     res.send(accessToken)
-        // })
 
         // load  product api
         app.get('/product', async (req, res) => {
@@ -98,24 +74,7 @@ async function run() {
             const result = await productCollection.insertOne(newProduct)
             res.send(result)
         })
-
-
-        // app.get('/items', verifyJWT, async (req, res) => {
-        //     const decodedEmail = req.decoded.email
-        //     console.log(decodedEmail)
-        //     const email = req.query.email
-        //     console.log(email)
-        //     if (decodedEmail === email) {
-        //         const query = { email: email }
-        //         const cursor = productCollection.find(query)
-        //         const result = await cursor.toArray()
-        //         res.send(result)
-        //     }
-        //     else {
-        //         return res.status(403).send({ message: 'Forbidden access' })
-        //     }
-        // })
-
+        //get my items api
         app.get('/items', async (req, res) => {
             const email = req.query.email
             console.log(email)
